3b09538983dc837267a4346de1975565da8e72da
pclevy <pclevybr@gmail.com>
2019-06-20 19:01:23 -0300

    testGlobalStyle: 'font-face' resolvido (public/fonts)

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -5,53 +5,24 @@ export const GlobalStyles = createGlobalStyle`
     font-family: 'Helvetica Neue Bold';
     font-style: italic;
     font-weight: normal;
-    src: local('Helvetica Neue Bold'), url('./assets/fonts/HelveticaNeueBold.woff') format('woff');
+    src: local('Helvetica Neue Bold'), url('./fonts/HelveticaNeueBold.woff') format('woff');
   }
   @font-face {
     font-family: 'Helvetica Neue Medium';
     font-style: normal;
     font-weight: normal;
-    src: url('./assets/fonts/HelveticaNeue-Medium.woff') format('woff');
+    src: url('./fonts/HelveticaNeue-Medium.woff') format('woff');
   }
 
   @font-face {
-    font-family: 'AguafinaScript-Regular';
+    font-family: 'Agua Fina Script Regular';
     font-style: normal;
     font-weight: normal;
-    src: url('./assets/fonts/AguafinaScript-Regular.ttf') format('truetype');
+    src: url('./fonts/AguafinaScript-Regular.ttf') format('truetype');
   }
 
 h1 {
-  font-family: "AguafinaScript-Regular";
-  font-size: 12px;
+  font-family: "Agua Fina Script Regular";
+  font-size: 18px;
 }  
 `
-
-//font-family: "Helvetica Neue Bold";
-//font-family: "Arial";
-
-
-//export default GlobalStyles;
-
-{/*
-  USO:
-  import { GlobalStyle } from './styles/global'
-
-class App extends Component {
-  render() {
-
-    return (
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle/>
-          <Container/>
-        </>
-      </ThemeProvider>
-
-    )
-  }
-}
-
-      //url(${font}) format('wolfenstein'),
-
-*/}
